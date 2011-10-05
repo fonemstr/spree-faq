622bf67dcc597524ec0a3d0b81ab753ecd2b1dda
David Demers <ddemers@etelephonesystems.com>
2011-10-05 20:40:23 -0300

    Edited lib/spree_faq.rb via GitHub

--- lib/spree_faq.rb
+++ lib/spree_faq.rb
@@ -9,6 +9,10 @@ module SpreeFaq
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
         Rails.env.production? ? require(c) : load(c)
       end
+      
+      Dir.glob(File.join(File.dirname(__FILE__), "../app/overrides/**/*.rb")) do |c|
+        Rails.application.config.cache_classes ? require(c) : load(c)
+      end
     end
 
     config.to_prepare &method(:activate).to_proc
