67ceefd63a3e117e89cb4d6ebdfdb91d72dc4004
David Demers <ddemers@etelephonesystems.com>
2011-10-05 20:45:24 -0300

    Edited app/overrides/add_faq_admin_tab.rb via GitHub

--- app/overrides/add_faq_admin_tab.rb
+++ app/overrides/add_faq_admin_tab.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => "layouts/admin",
                      :name => "add blog admin menu tab",
                      :insert_bottom => "[data-hook='admin_tabs']",
-                     :text => %q{<%=  tab(:question_categories, :label => :question_categories_admin)  %>}
+                     :text => %q{<%= tab(:question_categories, :label => :question_categories_admin) %>})
