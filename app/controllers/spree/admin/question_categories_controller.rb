723afdd0e6061b97024a6e44e5ff7d594a3f1e24
David Demers <fonemstr@gmail.com>
2012-08-13 08:41:27 -0400

    add spree folders

---
+++ app/controllers/spree/admin/question_categories_controller.rb
@@ -0,0 +1,15 @@
+  class Spree::Admin::QuestionCategoriesController < Admin::ResourceController
+    #resource_controller
+
+    helper 'spree/base'
+
+    new_action.response do |format|
+      format.html {render :action => :new, :layout => false}
+    end
+    update.response do |format|
+      format.html { redirect_to admin_question_categories_path }
+    end
+    create.response do |format|
+      format.html { redirect_to edit_admin_question_category_path(@question_category) }
+    end
+  end
--- app/overrides/add_faq_admin_tab.rb
+++ app/overrides/add_faq_admin_tab.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/admin/configurations/index",
                      :name => "add_faq_configuration_line",
                      :insert_bottom => "[data-hook='admin_configurations_menu']",
-                     :text => "<%= configurations_menu_item("Faq", admin_question_categories_url, "Manage Faq Questions") %>",
+                     :text => "<%= configurations_menu_item('Faq', admin_question_categories_url, 'Manage Faq Questions') %>",
                      :disabled => false)
