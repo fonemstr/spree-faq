cd7e581e3b60849d0c67ce0cfc09b4e8353a5d40
David Demers <fonemstr@gmail.com>
2012-12-19 10:17:35 -0500

    deleted unneeded files

--- app/assets/javascripts/store/spree_faq.js
+++
@@ -1,8 +0,0 @@
- $(function() {
-    $('.answer').hide();
-    $('.question').click(function() {
-      var id = $(this).attr('id').split('_')[1];
-      $('#answer_' + id).slideToggle();  
-      return false;
-    });
-  });
