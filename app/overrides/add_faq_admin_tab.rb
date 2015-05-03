5370c86a251de01b8e7fbe8b3aad15f4f0adb315
David Demers <fonemstr@gmail.com>
2015-05-03 19:36:16 -0400

    add faq to admin sidebar

--- app/overrides/add_faq_admin_tab.rb
+++ app/overrides/add_faq_admin_tab.rb
@@ -1,5 +1,6 @@
-Deface::Override.new(:virtual_path => %q{spree/admin/shared/_configuration_menu},
-                     :name => %q{add_faq_configuration_line},
-                     :insert_bottom => %q{[data-hook="admin_configurations_sidebar_menu"]},
-                     :text => %q{<%= configurations_sidebar_menu_item t(:question_categories_admin), admin_question_categories_url %>},
-                     :disabled => false)
+Deface::Override.new(
+  virtual_path: 'spree/layouts/admin',
+  name: 'faq_admin_sidebar_menu',
+  insert_bottom: '#main-sidebar',
+  partial: 'spree/admin/shared/faq_sidebar_menu'
+)
